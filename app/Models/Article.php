e1d7b1d84f511ee1a4ef1732f724b69093978bd9
ekoenitz <ErikKoenitzer@gmail.com>
2024-01-12 14:58:56 +0900

    Article seeder and model

---
+++ app/Models/Article.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\Enums\Genres;
+
+class Article extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'content',
+        'genre',
+        'total_views',
+        'author_id',
+        'created_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'genre' => Genres::class,
+    ];
+}
