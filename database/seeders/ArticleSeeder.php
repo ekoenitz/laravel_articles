8cf8eff0b358a6e94b8e8b53e4c643057699df25
ekoenitz <ErikKoenitzer@gmail.com>
2024-01-23 12:06:00 +0900

    Allow longer articles

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -71,6 +71,28 @@ class ArticleSeeder extends Seeder
                 'author_id' => 2,
                 'created_at' => Carbon::now(),
             ],
+            [
+                'id' => 7,
+                'title' => 'Long News A',
+                'description' => 'Long article by A, sourced from https://en.wikipedia.org/wiki/Battle_of_Chikhori',
+                'content' => "The Battle of Chikhori was fought between the armies of King George VIII of Georgia and the rebellious nobles led by a royal kinsman Bagrat in 1463. It took place near the fortress Chikhori in the district of Argveti in western Georgia, and ended in the king's decisive defeat.\n
+                The battle was the culmination of a fierce and extended internal struggle for hegemony in Georgia that began after the brief reign of Vakhtang IV (1442-1446), and would eventually end with fission of the kingdom. The rebellion against Vakhtang’s brother and successor George VIII, whose legitimacy was disputed, was fomented in western and southern provinces of the Kingdom of Georgia. The western Georgian dukes rallied behind George’s relative Bagrat in a powerful coalition and met the royal army on the battlefield at Chikhori. George was defeated, and Bagrat was crowned king of Imereti at Kutaisi. But in return for their aid, the new monarch was obliged to create a principality for each of his four major allies. Henceforth, the Gelovani family in Svaneti, the Sharvashidze in Abkhazia, the Dadiani in Mingrelia, and the Vardanidze-Gurieli in Guria ruled as semi-independent princes.\n
+                Odishi (Mingrelia), Guria, Abkhazia, Svaneti and Samtskhe – dominated by their own feudal clans.",
+                'genre' => Genres::NEWS,
+                'author_id' => 1,
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'id' => 8,
+                'title' => 'Long News B',
+                'description' => 'Long article by B, sourced from https://en.wikipedia.org/wiki/Battle_of_Chikhori',
+                'content' => "The Battle of Chikhori was fought between the armies of King George VIII of Georgia and the rebellious nobles led by a royal kinsman Bagrat in 1463. It took place near the fortress Chikhori in the district of Argveti in western Georgia, and ended in the king's decisive defeat.\n
+                The battle was the culmination of a fierce and extended internal struggle for hegemony in Georgia that began after the brief reign of Vakhtang IV (1442-1446), and would eventually end with fission of the kingdom. The rebellion against Vakhtang’s brother and successor George VIII, whose legitimacy was disputed, was fomented in western and southern provinces of the Kingdom of Georgia. The western Georgian dukes rallied behind George’s relative Bagrat in a powerful coalition and met the royal army on the battlefield at Chikhori. George was defeated, and Bagrat was crowned king of Imereti at Kutaisi. But in return for their aid, the new monarch was obliged to create a principality for each of his four major allies. Henceforth, the Gelovani family in Svaneti, the Sharvashidze in Abkhazia, the Dadiani in Mingrelia, and the Vardanidze-Gurieli in Guria ruled as semi-independent princes.\n
+                Odishi (Mingrelia), Guria, Abkhazia, Svaneti and Samtskhe – dominated by their own feudal clans.",
+                'genre' => Genres::NEWS,
+                'author_id' => 2,
+                'created_at' => Carbon::now(),
+            ]
         ], 'id');
     }
 }
