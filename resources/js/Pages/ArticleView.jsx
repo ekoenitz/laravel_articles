20b13232233f203a1b480b91a7e04a9887766da6
ekoenitz <ErikKoenitzer@gmail.com>
2024-01-23 11:46:29 +0900

    Use article title as page title

--- resources/js/Pages/ArticleView.jsx
+++ resources/js/Pages/ArticleView.jsx
@@ -15,11 +15,10 @@ function renderArticles(articles) {
 }
 
 export default function ArticleView({ auth, article }) {
-    console.log("ARTICLE", article)
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">{article.title}</h2>}
         >
             <Head title="Article" />
 
