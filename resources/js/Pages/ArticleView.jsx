be3084b205db9e13bca73df56b874b957cae96eb
ekoenitz <ErikKoenitzer@gmail.com>
2024-01-23 11:56:08 +0900

    Display article content

--- resources/js/Pages/ArticleView.jsx
+++ resources/js/Pages/ArticleView.jsx
@@ -22,6 +22,17 @@ export default function ArticleView({ auth, article }) {
         >
             <Head title="Article" />
 
+            <div className="pb-3"/>
+            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="bg-white bg-auto hover:bg-sky-100 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div className="font-bold px-6 pt-3 pb-1 text-gray-900">{article.description}</div>
+                    <div className="px-6 pt-1 pb-3 text-gray-400 text-sm">
+                        {`${article.genre} | ${article.author_name} | ${article.created_at}`}
+                    </div>
+                    <div className="px-6 pt-1 pb-3 text-gray-900">{article.content}</div>
+                </div>
+            </div>
+
         </AuthenticatedLayout>
     );
 }
