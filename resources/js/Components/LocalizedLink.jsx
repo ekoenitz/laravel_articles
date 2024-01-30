c82065f9752ba5f3999e1f00b285f26bd75cb64b
ekoenitz <ErikKoenitzer@gmail.com>
2024-01-30 16:13:04 +0900

    Pass all props to child Link component

--- resources/js/Components/LocalizedLink.jsx
+++ resources/js/Components/LocalizedLink.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from '@inertiajs/react';
 import { useTranslation } from 'react-i18next';
 
-export default function LocalizedLink({path, args, children}) {
+export default function LocalizedLink(props) {
+    const {path, args, children} = props;
     const { i18n } = useTranslation();
     return (
-        <Link href={route(path, [i18n.language].concat(args))}>
+        <Link href={route(path, [i18n.language].concat(args))} {...props}>
             {children}
         </Link>
     )
