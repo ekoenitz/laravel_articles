3481a91539b41ede8bb4840442f0ea8df7aa03c9
ekoenitz <ErikKoenitzer@gmail.com>
2024-02-01 15:35:12 +0900

    Include article view count in ArticleInfo

--- resources/js/Components/ArticleInfo.jsx
+++ resources/js/Components/ArticleInfo.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 export default function ArticleInfo({article}) {
-    const { i18n } = useTranslation();
+    const { t } = useTranslation();
         return (
         <div className="px-6 pt-1 pb-3 text-gray-400 text-sm">
-            {`${article.genre} | ${article.author_name} | ${article.created_at}`}
+            {`${article.views} ${t("articleInfo.views")} | ${article.genre} | ${article.author_name} | ${article.created_at}`}
         </div>
     );
 }
