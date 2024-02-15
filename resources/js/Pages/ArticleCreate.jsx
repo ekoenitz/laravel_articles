bfaba2c5b30db7bfad2de93437f80cd4f7844cb3
ekoenitz <ErikKoenitzer@gmail.com>
2024-02-15 16:26:21 +0900

    Add inputs for title, desc, content

--- resources/js/Pages/ArticleCreate.jsx
+++ resources/js/Pages/ArticleCreate.jsx
@@ -2,10 +2,19 @@ import ArticleInfo from '@/Components/ArticleInfo';
 import ArticleListing from '@/Components/ArticleListing';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+
+const updateValue = (setFunction) => (event) => {
+    setFunction(event.target.value);
+}
 
 export default function ArticleView({ auth }) {
     const { t } = useTranslation();
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [content, setContent] = useState("");
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -15,7 +24,23 @@ export default function ArticleView({ auth }) {
 
             <div className="pb-3"/>
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div className="bg-white bg-auto hover:bg-sky-100 overflow-hidden shadow-sm sm:rounded-lg"/>
+                <div className="bg-white bg-auto hover:bg-sky-100 overflow-hidden shadow-sm sm:rounded-lg">
+                <input 
+                    className="w-33 p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                    defaultValue={title}
+                    onChange={updateValue(setTitle)}
+                />
+                <input 
+                    className="w-33 p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                    defaultValue={description}
+                    onChange={updateValue(setDescription)}
+                />
+                <input 
+                    className="w-33 p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                    defaultValue={content}
+                    onChange={updateValue(setContent)}
+                />
+                </div>
             </div>
 
         </AuthenticatedLayout>
